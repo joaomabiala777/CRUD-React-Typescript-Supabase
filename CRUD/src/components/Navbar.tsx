f5b0575810db3bcebbbf2a15d655ea62e3220c24
João Cumbo Mabiala <154311604+joaomabiala777@users.noreply.github.com>
2026-03-11 13:31:25 +0100

    Typei o Navbar

--- CRUD/src/components/Navbar.tsx
+++ CRUD/src/components/Navbar.tsx
@@ -1,12 +1,12 @@
-const Navbar = () => {
+import React from 'react'; // Importação necessária em algumas versões/configurações
+
+const Navbar: React.FC = () => {
   return (
-    <div>
-      <nav className="bg-amber-300 shadow-md fixed top-0 left-0 w-full z-50">
-        <div className="max-w-screen flex items-center justify-center mx-auto p-4">
-          <h1 className="text-2xl font-bold">Gestão de Stock</h1>
-        </div>
-      </nav>
-    </div>
+    <nav className="bg-amber-300 shadow-md fixed top-0 left-0 w-full z-50">
+      <div className="max-w-screen flex items-center justify-center mx-auto p-4">
+        <h1 className="text-2xl font-bold">Gestão de Stock</h1>
+      </div>
+    </nav>
   );
 };
 
